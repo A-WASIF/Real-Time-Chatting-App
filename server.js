920f08aa2e62a8faea2cf22e02d0ae8dd338a95f
WASIF <wasifaliwa.09@gmail.com>
2024-07-06 21:57:55 +0530

    second commit

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ io.on('connection', socket => {
     })
 });
 
-server.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
 });
