585690055f0968bd39d03b473a139ec5e5b6996d
WASIF <wasifaliwa.09@gmail.com>
2024-07-06 21:18:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const { measureMemory } = require('vm');
+
+const app = express();
+app.use(cors());  // Enable CORS for all routes
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",  // Allow requests from this origin
+        methods: ["GET", "POST"]
+    }
+});
+
+const users = {};
+
+
+io.on('connection', socket => {
+    console.log("New User Connected");
+
+    socket.on('new-user-joined', Name => {
+        users[socket.id] = Name;
+        socket.broadcast.emit('user-join-message', Name)
+    })
+
+
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', {
+            Name : users[socket.id],
+            message : message
+        });
+    })
+
+
+    socket.on('disconnect', ()=> {
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
+    })
+});
+
+server.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on port 3000');
+});
